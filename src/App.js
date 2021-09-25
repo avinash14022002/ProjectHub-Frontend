1fd9d60b4fafad4b34a6025335907425418d66e9
Anubhav Pabby <legendtheanubhav@gmail.com>
2021-09-25 16:29:07 +0530

    Designed Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import Navbar from './components/navbar/Navbar.js';
-import Footer from './components/footer/Footer.js';
+import React from 'react';
 import './App.css';
+import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
+import About from './components/pages/about-us/About'
+import Contact from './components/pages/contact/Contact'
+import Home from './components/pages/home/Home'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const App = () => {
-    return (
+function App() {
+  return (
+    <Router>
       <div className="page-container">
         <div className="content-wrap">
           <Navbar />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/contact' component={Contact} />
+          </Switch>
         </div>
         <Footer />
       </div>
-    );
-};
-  
+    </Router>
+  );
+}
+
 export default App;
