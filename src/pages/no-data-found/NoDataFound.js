38ba84a14a23de7a067e057bfed5912c46be6df9
Anubhav Pabby <legendtheanubhav@gmail.com>
2022-01-14 16:50:51 +0530

    Implemented No-Data found page

--- src/pages/no-data-found/NoDataFound.js
+++ src/pages/no-data-found/NoDataFound.js
@@ -2,12 +2,12 @@ import React from 'react';
 import NoDataImage from "../../assets/no-data-found/noDataFound.png";
 import './NoDataFound.css';
 
-const NoDataFound = ({ props }) => {
+const NoDataFound = ({ displayMessage }) => {
     
     return(
         <div>
             <img className="noDataImage" src={NoDataImage} height={350} width={560} alt='No Data Found'/>
-            <h1 className="messageToDisplay">{props}</h1>
+            <h1 className="messageToDisplay">{displayMessage}</h1>
         </div>
     );
 };
